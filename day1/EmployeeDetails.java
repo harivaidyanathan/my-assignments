0fb8148e10b425283cd391d65861b7f64c32539b
harivaidyanathan <harivaidyanathan@gmail.com>
2025-08-16 22:24:39 +0530

    Add files via upload

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		byte age = 30;
+		Double salary = 55000.75;
+		char gender = 'M';
+		Boolean acceptTerms = true;
+		float rating = 4.5f;
+		Long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("name is "+name);
+		System.out.println("age is "+age);
+		System.out.println("salary is "+salary);
+		System.out.println("gender is "+gender);
+		System.out.println("acceptTerms is"+acceptTerms);
+		System.out.println("rating is "+rating);
+		System.out.println("mobileNumber is "+mobileNumber);
+		System.out.println("pinCode is "+pinCode);
+		System.out.println("luckyNumber is "+luckyNumber);
+		
+
+	}
+
+}
